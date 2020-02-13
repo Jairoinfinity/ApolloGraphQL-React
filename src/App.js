91326b0a17b7672e7fac4bb8a7aa3221484f988f
Jairo <jairobernaldev@gmail.com>
2020-02-13 12:34:56 +0100

    Fin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ApolloClient, { gql } from 'apollo-boost';
+import { ApolloProvider, useQuery } from '@apollo/react-hooks';
+
+const client = new ApolloClient({
+  uri: 'https://rickandmortyapi.com/graphql/'
+});
+
+const CharactersQuery = () => {
+  const { loading, error, data } = useQuery(gql`{
+    characters{
+      results{
+        id
+        name
+        image
+      }
+    }
+  }`);
+
+  if (loading) return(
+  <div>
+    <img className="App-logo" src="https://rickandmortyapi.com/api/character/avatar/103.jpeg" alt="Loading"/>
+    <p>Loading...</p>
+  </div>
+  )
+  if (error) return <p>Error!</p>
+
+  return data.characters.results.map(character => {
+    return (
+      <div>
+        <img className="App-logo-characters" src={character.image} alt="Loading"/>
+        <p>{character.name}</p>
+      </div>
+      )
+  })
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+      <ApolloProvider client={client}>
+        <CharactersQuery></CharactersQuery>
+      </ApolloProvider>
     </div>
   );
 }
